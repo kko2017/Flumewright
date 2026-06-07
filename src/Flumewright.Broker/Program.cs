ec44a3c9508e037155c64dc83429ee1ce4af098f
ALEXOH <oh.kwangeun8@gmail.com>
2026-06-07 08:17:18 -0300

    feat(broker): plaintext gRPC MessageBus host backed by in-memory store

--- src/Flumewright.Broker/Program.cs
+++ src/Flumewright.Broker/Program.cs
@@ -1,7 +1,22 @@
-using Flumewright.Broker;
+using Flumewright.Broker.Core;
+using Flumewright.Broker.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+var builder = WebApplication.CreateBuilder(args);
 
-var host = builder.Build();
-host.Run();
+// M1: plaintext HTTP/2 (h2c). mTLS/HTTPS is M4.
+builder.WebHost.ConfigureKestrel(options =>
+{
+    var port = builder.Configuration.GetValue<int>("Broker:Port", 5050);
+    options.ListenAnyIP(port, listen => listen.Protocols = HttpProtocols.Http2);
+});
+
+builder.Services.AddGrpc();
+builder.Services.AddSingleton<ITopicStore, InMemoryTopicStore>();
+
+var app = builder.Build();
+
+app.MapGrpcService<MessageBusService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client.");
+
+app.Run();
