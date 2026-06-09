793bafcd4e90c9825e3ffd9b5f89587f1822d692
ALEXOH <oh.kwangeun8@gmail.com>
2026-06-08 23:11:46 -0300

    feat(samples): wire SamplePublisher/Subscriber to SDK (drop unneeded h2c switch)

--- samples/SamplePublisher/Program.cs
+++ samples/SamplePublisher/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+using Flumewright.Client;
+using Grpc.Net.Client;
+
+
+
+using var publisher = new FlumewrightPublisher("http://localhost:5050");
+var offset = await publisher.PublishAsync("demo", System.Text.Encoding.UTF8.GetBytes("hello"));
+Console.WriteLine($"published at offset {offset}");
